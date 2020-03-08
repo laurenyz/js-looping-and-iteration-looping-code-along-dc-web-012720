df7270a9d13418d73d0b1a3223cb1978c0b25d8b
Lauren Yu <laurenkyu@gmail.com>
2020-03-08 21:40:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function wrapGift(names, event){
   for (let i = 0; i < names.length; i++){
     arr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
   }
-  console.log(arr);
+  return arr;
 }
 
 wrapGift( ["Audrey", "Lauren"], "birthday")
