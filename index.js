4727d1f84184c429520cef295a1b0774b76ab656
Lauren Yu <laurenkyu@gmail.com>
2020-03-08 21:30:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+let arr = []
+
 function wrapGift(array, event){
   for (let i = 0; i < array.length; i++){
-    `Thank you, ${array[i]}, for the wonderful ${event} gift!`
+    arr.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
   }
+  return arr;
 }
