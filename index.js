2719b8b9246c3cef5c2856251760a4b225312db7
Lauren Yu <laurenkyu@gmail.com>
2020-03-08 21:38:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let arr = ["Audrey", "Lauren"]
 
 function wrapGift(names, event){
-  for (let i = 0; i < array.length; i++){
+  for (let i = 0; i < names.length; i++){
     arr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
   }
   console.log(arr);
